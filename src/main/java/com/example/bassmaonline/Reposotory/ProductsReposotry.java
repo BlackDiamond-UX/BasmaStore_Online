888f32908fd228bf332190e2bc9a56284ae97b2b
Rouiha Ayoub <58300988+BlackDiamond-UX@users.noreply.github.com>
2021-05-04 15:47:57 +0000

    delete all files

--- src/main/java/com/example/bassmaonline/Reposotory/ProductsReposotry.java
+++
@@ -1,8 +0,0 @@
-package com.example.bassmaonline.Reposotory;
-
-import com.example.bassmaonline.entity.ProductsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsReposotry extends JpaRepository<ProductsEntity, Long> {
-    ProductsEntity findByIdProd(Long id);
-}
