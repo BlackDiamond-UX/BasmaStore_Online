cf821625fa92cb70379fbc864032eee3e52edc7d
Rouiha Ayoub <58300988+BlackDiamond-UX@users.noreply.github.com>
2021-05-04 15:52:28 +0000

    added new files

---
+++ src/main/java/com/example/bassmaonline/BassmaOnlineApplication.java
@@ -0,0 +1,14 @@
+package com.example.bassmaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BassmaOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BassmaOnlineApplication.class, args);
+    }
+
+}
